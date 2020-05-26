19841dccd8cf553ed400783bc946e3a787498b1e
ahmet-uzgor <ahmetuzgor10@gmail.com>
2020-05-26 18:55:34 +0300

    Stacks are built with Arrays and methods completed

--- stacks-queues/stacks.js
+++ stacks-queues/stacks.js
@@ -63,3 +63,34 @@ myStack.push('Medium');
 myStack.pop();
 myStack.peek();
 console.log(myStack);
+
+
+//Stacks creation with Arrays;
+class Stack2{ // it creates stack which is LIFO => Last in First Out
+    constructor(){
+        this.length = 0;
+        this.arr1 = [];
+    }
+    peek(){ // it shows the element which is in the top
+        return this.arr1[this.length-1];     
+    }
+    push(value){ // It adds data to the top of stacks
+        this.arr1.push(value);
+        this.length++;
+    }
+    pop(){ // it removes last element of stacks
+        if(this.length===0){
+            console.log("There is no element to remove");
+        }
+        delete this.arr1[this.length-1];
+        this.length--;
+    }
+}
+
+const myStack2 = new Stack2();
+myStack2.push('1');
+myStack2.push('2');
+myStack2.push('3');
+console.log(myStack2);
+myStack2.pop();
+console.log(myStack2.peek()) ;

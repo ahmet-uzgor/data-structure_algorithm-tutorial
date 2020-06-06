9c92acddee95b4b123d4b591fa38056513791ef6
ahmet-uzgor <ahmetuzgor10@gmail.com>
2020-06-06 13:26:03 +0300

    BST insert method and traverse funtion which is well print are created

--- Trees/binary-search-trees.js
+++ Trees/binary-search-trees.js
@@ -12,14 +12,49 @@ class Node{
 
 class BinarySearchTrees{
     constructor(){
-        this.root = null ;
+        this.root = null;
     }
     insert(value){
-
+        const newNode =new Node(value);
+        if(this.root === null){
+            this.root = newNode
+        }else{
+            let scanNode = this.root ;
+            while(true){
+                if(value < scanNode.value){
+                    //Add to left of node
+                    if(!scanNode.left){
+                        scanNode.left = newNode ;
+                        return this;
+                    }
+                    scanNode = scanNode.left;
+                }else{
+                    //Add to right of node
+                    if(!scanNode.right){
+                        scanNode.right = newNode ;
+                        return this;
+                    }
+                    scanNode = scanNode.right ;
+                }
+            }
+        }
     }
     lookup(value){
 
     }
 }
 
-const newTree = new BinarySearchTrees() ;
+const newTree = new BinarySearchTrees();
+newTree.insert(15);
+newTree.insert(13);
+newTree.insert(17);
+JSON.stringify(traverse(newTree.root));
+
+function traverse(node){
+    const tree = {value: node.value};
+    tree.left = node.left === null ? null :
+    traverse(node.left);
+    tree.right = node.right === null ? null :
+    traverse(node.right);
+    return tree;
+}

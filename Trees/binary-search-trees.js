0284b46a2706758c0d1a6f4a21fa529740db9567
ahmet-uzgor <ahmetuzgor10@gmail.com>
2020-06-06 12:34:14 +0300

    Binary Search Trees are built with classes

---
+++ Trees/binary-search-trees.js
@@ -0,0 +1,25 @@
+// Root -- Parent -- Child -- Leaf -- Sibling
+// Big O ==> O(logN) BST --> Binary Search Trees
+// Balnced and Unbalanced BSTs are evaluated
+
+class Node{
+    constructor(value){
+        this.value = value;
+        this.left = null ;
+        this.right = null ;
+    }
+}
+
+class BinarySearchTrees{
+    constructor(){
+        this.root = null ;
+    }
+    insert(value){
+
+    }
+    lookup(value){
+
+    }
+}
+
+const newTree = new BinarySearchTrees() ;

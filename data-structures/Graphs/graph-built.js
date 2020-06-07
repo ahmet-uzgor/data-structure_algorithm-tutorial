f3bdf5011a04ae3f55a18bf1c4a1d959d8ba0acf
ahmet-uzgor <ahmetuzgor10@gmail.com>
2020-06-07 11:41:04 +0300

    graph creation with classes and methods are written

---
+++ data-structures/Graphs/graph-built.js
@@ -0,0 +1,48 @@
+class Graph{ // Undirected - Unweighted Graph Creation
+    constructor(){
+        this.numberOfNodes = 0;
+        this.adjacentList = {};
+    }
+    addVertex(node){
+        this.adjacentList[node] = [];
+        this.numberOfNodes++;
+    }
+    addEdge(node1,node2){
+        //Undirected Graph
+        this.adjacentList[node1].push(node2);
+        this.adjacentList[node2].push(node1);
+    }
+    showConnections(){ // it shows all connections between nodes . AdjacentList keeps connection data.
+        const allNodes =  Object.keys(this.adjacentList);
+        for(let node of allNodes){
+            let nodeConnections = this.adjacentList[node];
+            let connections = "";
+            let vertex ;
+            for(vertex of nodeConnections){
+                connections += vertex + " ";
+            }
+            console.log(node + "-->" + connections);
+        }
+    }
+}
+
+const newGraph = new Graph();
+newGraph.addVertex('0');
+newGraph.addVertex('1');
+newGraph.addVertex('2');
+newGraph.addVertex('3');
+newGraph.addVertex('4');
+newGraph.addVertex('5');
+newGraph.addVertex('6');
+newGraph.addVertex('7');
+newGraph.addEdge('0','1');
+newGraph.addEdge('0','2');
+newGraph.addEdge('1','2');
+newGraph.addEdge('1','3');
+newGraph.addEdge('2','4');
+newGraph.addEdge('3','4');
+newGraph.addEdge('3','5');
+newGraph.addEdge('4','6');
+newGraph.addEdge('5','6');
+newGraph.addEdge('6','7');
+newGraph.showConnections();

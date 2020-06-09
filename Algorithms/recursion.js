cb51a1aae0c17492e5d08c7f9c34a76fd0c5b882
Ahmet üzgör <63739083+ahmet-uzgor@users.noreply.github.com>
2020-06-09 14:26:43 +0300

    Recursion function is written and notes are added

--- Algorithms/recursion.js
+++ Algorithms/recursion.js
@@ -1,1 +1,18 @@
-// Recursive functions
+// Recursive functions - Example Recursion 
+
+let counter = 0;
+function recursiveFunc(){
+  console.log(counter);
+  if(counter >3){
+    return 'done!';
+  }
+  counter++
+  return recursiveFunc(); // it provides return data to be able to take return value of recursiveFunc() 
+}
+
+recursiveFunc(); // when it called , it call again itself inside of function
+
+//3 rules of recursion
+// 1. Identify the base case
+// 2. Identify the recursive case
+// 3. Get closer and closer and return when needed. Usually it needs 2 returns

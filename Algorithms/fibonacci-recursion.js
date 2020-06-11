082595953f8cbf7ad188ec8e6756ff746c135054
ahmet-uzgor <ahmetuzgor10@gmail.com>
2020-06-11 11:32:41 +0300

    Iterative solutions and notes are added

--- Algorithms/fibonacci-recursion.js
+++ Algorithms/fibonacci-recursion.js
@@ -4,7 +4,7 @@
 // the pattern of the sequence is that each value is the sum of 
 // the 2 previous values, that means that for N = 8 --> 3 + 5
 
-function findFibonacciRecursive(n){
+function findFibonacciRecursive(n){ // O(2^n) exponentially increasing ;recursive function
     if(n < 2){
         return n;
     }
@@ -12,6 +12,12 @@ function findFibonacciRecursive(n){
 }
 console.log(findFibonacciRecursive(8));
 
-function findFibonacciIterative(){
-    //code here
+function findFibonacciIterative(){ // O(n) this solution  is better than recursive one.
+    let arr = [0, 1];
+    for(let i = 2 ; i < n+1 ; i++){
+        arr.push(arr[i-2] + arr[i-1]);
+    }
+    return arr[n]
 }
+
+console.log("Iterative solution answer is ", findFibonacciIterative(8));

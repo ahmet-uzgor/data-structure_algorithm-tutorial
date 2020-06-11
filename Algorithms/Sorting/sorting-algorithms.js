b9127565f091f775cb588e6edafa678b8213499f
ahmet-uzgor <ahmetuzgor10@gmail.com>
2020-06-11 17:23:10 +0300

    Sorting folder is created for BFS & DFS

---
+++ Algorithms/Sorting/sorting-algorithms.js
@@ -0,0 +1,5 @@
+// There are thousands of sorting algorithms beside the built-in methods
+// BFS + DFS will be written in there
+
+const letters = ['a', 'f', 'b', 'z', 'c', 'e'];
+letters.sort() ; // This built-in methods sort the elements of arrays

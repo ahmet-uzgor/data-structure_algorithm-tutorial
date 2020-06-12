4831d3fe26c8a6b47231f97e1ecebefe595efa9a
ahmet-uzgor <ahmetuzgor10@gmail.com>
2020-06-12 12:17:46 +0300

    Heap Sort & Non-comparison sort algorithm notes are added

---
+++ Algorithms/Sorting/heap-non-comparison sort.js
@@ -0,0 +1,9 @@
+// Heap Sorting time complexity is O(nlogn) space complexity is O(1)
+// Sources to learn details ==> https://stackoverflow.com/questions/2467751/quicksort-vs-heapsort
+//For heap sort ==> https://brilliant.org/wiki/heap-sort/
+
+// Non-Comparison sorts are only works with integer numbers that is limited an integer
+// It does not compare just put elements into a queue regarding mass.
+// Sources ==> https://brilliant.org/wiki/radix-sort/
+// Sources ==> https://brilliant.org/wiki/counting-sort/
+// Sources ==> https://www.cs.usfca.edu/~galles/visualization/CountingSort.html

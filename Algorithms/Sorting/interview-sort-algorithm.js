7f394afe2b255f1b48136c960b87190ca26d8d98
Ahmet üzgör <63739083+ahmet-uzgor@users.noreply.github.com>
2020-06-12 12:27:48 +0300

    Create interview-sort-algorithm.js

---
+++ Algorithms/Sorting/interview-sort-algorithm.js
@@ -0,0 +1,15 @@
+//#1 - Sort 10 schools around your house by distance:
+
+//#2 - eBay sorts listings by the current Bid amount:
+
+//#3 - Sport scores on ESPN
+
+//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data
+
+//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews
+
+//#6 - Temperature Records for the past 50 years in Canada
+
+//#7 - Large user name database needs to be sorted. Data is very random.
+
+//#8 - You want to teach sorting for the first time

4295b8b1325d4ab2e8cc88cbfbbbe18f547c70bb
ahmet-uzgor <ahmetuzgor10@gmail.com>
2020-06-13 05:39:59 +0300

    memorized function with closure is written

---
+++ Algorithms/Dynamic-Programming/cache-func-closure.js
@@ -0,0 +1,19 @@
+// To use cache inside of function in JS we have closure so always pattern is same to do this.
+
+
+function memoizedAddTo80(number){
+    let cache = {};
+    return function(number){
+        if(number in cache){
+            console.log("Answer from cache");
+            return cache[number];
+        }else{
+            cache[number] = number + 80 ;
+            return cache[n]; 
+        }
+    }
+}
+const memoized = memoizedAddTo80(); // it provides us to remember cache even cache inside of func.
+
+console.log('First Call for 75', memoized(75));
+console.log('Second Call for 75', memoized(75));
